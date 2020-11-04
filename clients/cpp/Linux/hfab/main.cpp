1f7678dd3e476dfc5b6bb179349e8728184e14b6
Jonas Forsslund <jonas@forsslundsystems.com>
2020-11-04 09:02:17 +0100

    progress

--- clients/cpp/Linux/hfab/main.cpp
+++ clients/cpp/Linux/hfab/main.cpp
@@ -11,6 +11,10 @@
 #include <string>
 #include <regex>
 
+
+#include <chrono>
+#include <thread>
+
 // ----- Portable keyboard loop break ------------------------------------------
 #ifdef WINDOWS
 #include <conio.h>
@@ -51,25 +55,30 @@ int main()
     hfab.open();
 
     int printcount=0;
-    while(!_kbhit()){
+    int max=1000;
+    while(!_kbhit() && max){
+        max--;
         fsVec3d p = hfab.getPos();
-        fsRot r = hfab.getRot();
-        if(++printcount%10000==0){
+        //fsRot r = hfab.getRot();
+        if(++printcount%100==0){
             //std::cout << toString(p) << " Main speed:" << hfab.getMsg().tD << " Escon speed:" << hfab.getMsg().tE << "\n";
-            std::string abc = hfab.getMsg().toString();
-            abc = std::regex_replace(abc,std::regex("\n"),"R");
+            //std::string abc = hfab.getMsg().toString();
+            //abc = std::regex_replace(abc,std::regex("\n"),"R");
 
-            //std::cout << "[012345678901234567890123456789012345678901234567890123456789123]\n";
+            //std::cout << "[1234567890123456789012345678901234567890123456789012345678901234] = LENGTH\n";
             //std::cout << "[" << abc << "]\n";
             position_hid_to_pc_message m = hfab.getMsg();
             //std::cout << m.tA;
-            std::cout << toString(p) << " " << m.tA << " " << m.lambda << " " << m.tD << " " << m.tE << "\n";
-            return 0;
+            std::cout << toString(p*1000) << " mm " << m.tA << " " << m.lambda << " " << m.tD << " " << m.tE << "\n";
+            //return 0;
 
         }
 
-        fsVec3d f = -200*p;
+        fsVec3d f = fsVec3d(0,0,0);//-100*p;
+        using namespace std::chrono_literals;
+        //std::this_thread::sleep_for(1000us);
         hfab.setForce(f);
+        //std::this_thread::sleep_for(100ms);
     }
 
     hfab.close();
