09147c9aa20a5a5197ddfc601b73ead5a1d29d89
Ola Forsslund <ola.forsslund@gmail.com>
2020-11-06 12:08:41 +0100

    Added connection monitor.

--- clients/cpp/Linux/stylus_client.cpp
+++ clients/cpp/Linux/stylus_client.cpp
@@ -86,6 +86,7 @@ int _kbhit(){
 // -----------------------------------------------------------------------------
 
 
+volatile bool clientGotLatestValue=false;
 
 
 // Battery Level UUID
@@ -160,37 +161,124 @@ void socket_server() {
 			   printf("Got new data in main loop: %02x %02x\n",bt_data[0],bt_data[1]);
 		   }
 		   */
+		clientGotLatestValue = true;
 	}
 	printf("Socket server quit.\n");
 }
 
+
+gatt_connection_t* g_connection;
+void* adapter=NULL;
+const char* device_address=NULL;
+uuid_t service_uuid;
+bool connectionActive=false;
+
 void notification_handler(const uuid_t* uuid, const uint8_t* data, size_t data_length, void* user_data) {
 	int i;
-
+	char str[256];
+	connectionActive=true;
 	//printf("Notification Handler: ");
-
+	
 	for (i = 0; i < data_length; i++) {
-		//printf("%02x ", data[i]);
+		g_log(NULL, G_LOG_LEVEL_DEBUG, "%02x ", data[i]);		
 		if(i<2)
 			bt_data[i]=data[i];
 	}
+	clientGotLatestValue=false;
 	//printf("\n");
 }
 
+
+
+bool deActivateConnection(){
+		if(g_connection==NULL){
+			printf("\nNo existing connection");
+			return false;
+		}
+
+		// Clean up old connection
+		gattlib_notification_stop(g_connection, &service_uuid);
+		gattlib_disconnect(g_connection);
+		g_connection=NULL;
+		clientGotLatestValue=false;
+		return false;
+}
+
+void disconnected_handler(void *){
+		static int nrDisconnects=0;
+		nrDisconnects++;		
+		connectionActive = false;
+		g_log(NULL, G_LOG_LEVEL_DEBUG, "Disconnected %i",nrDisconnects);
+		deActivateConnection();	
+}
+
+
+void activateConnection(){
+		if(g_connection!=NULL){
+			printf("\nExisting connection, skipping.");
+			return;
+		}
+		
+		g_connection = gattlib_connect(adapter, device_address, GATTLIB_CONNECTION_OPTIONS_LEGACY_DEFAULT);
+		if (g_connection == NULL) {
+			fprintf(stderr, "Fail to connect to the bluetooth device.\n");
+			return;
+		}
+
+		gattlib_register_notification(g_connection, notification_handler, NULL);
+		//gattlib_uuid_to_string(&service_uuid, uuid_str, sizeof(uuid_str));		
+		int ret = gattlib_notification_start(g_connection, &service_uuid);
+		if (ret) {
+			fprintf(stderr, "Fail to start notification.\n");
+			gattlib_disconnect(g_connection);			
+			puts("Done");
+			return;
+		}
+		gattlib_register_on_disconnect(g_connection, disconnected_handler, NULL);
+}
 
 static void on_user_abort(int arg) {
 	g_main_loop_quit(m_main_loop);
 }
 
+gboolean monitorConnection(gpointer user_data){
+	static int ideling;
+	if( clientGotLatestValue == false ){
+		if(g_connection != NULL){
+			ideling++;
+			if(ideling>10000){
+				disconnected_handler(NULL);
+			}
+		}
+	}
+	else
+	{
+		ideling=0;
+		if(g_connection==NULL)
+		{
+			activateConnection();
+		}
+	}
+	return true;	
+};
+
+void my_log_handler(const gchar *log_domain,
+             GLogLevelFlags log_level,
+             const gchar *message,
+             gpointer user_data){
+				 puts(message);
+			 }
+
+
 int main(int argc, char *argv[]) {
 	int ret;
-	gatt_connection_t* connection;
 
-	const char* adapter_name=NULL;
-	void* adapter;
-	const char* device_address;
-	char uuid_str[MAX_LEN_UUID_STR];
-	uuid_t service_uuid;
+	g_log_set_handler (NULL, GLogLevelFlags(G_LOG_LEVEL_DEBUG | G_LOG_LEVEL_WARNING | G_LOG_FLAG_FATAL
+                   			| G_LOG_FLAG_RECURSION), my_log_handler, NULL);
+
+
+	const char* adapter_name=NULL;		
+	char uuid_str[MAX_LEN_UUID_STR];	
 	gattlib_string_to_uuid("90ad0001-662b-4504-b840-0ff1dd28d84e", MAX_LEN_UUID_STR, &service_uuid);
 
 	if(argc==2){
@@ -211,23 +299,8 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	connection = gattlib_connect(adapter, device_address, GATTLIB_CONNECTION_OPTIONS_LEGACY_DEFAULT);
-	if (connection == NULL) {
-		fprintf(stderr, "Fail to connect to the bluetooth device.\n");
-		return 1;
-	}
-
-	gattlib_register_notification(connection, notification_handler, NULL);
-	gattlib_uuid_to_string(&service_uuid, uuid_str, sizeof(uuid_str));
-	puts(uuid_str);
-	ret = gattlib_notification_start(connection, &service_uuid);
-	if (ret) {
-		fprintf(stderr, "Fail to start notification.\n");
-		gattlib_disconnect(connection);
-		puts("Done");
-		return ret;
-	}
-
+	g_timeout_add(1, monitorConnection, NULL);
+		
 	// Catch CTRL-C
 	signal(SIGINT, on_user_abort);
 
@@ -238,13 +311,10 @@ int main(int argc, char *argv[]) {
 
 	run_server = false;
 	srv.join();
+	
+	deActivateConnection();
 
-	// In case we quit the main loop, clean the connection
-	gattlib_notification_stop(connection, &service_uuid);
 	g_main_loop_unref(m_main_loop);
-
-
-	gattlib_disconnect(connection);
 	puts("Done");
 	return ret;
 }
