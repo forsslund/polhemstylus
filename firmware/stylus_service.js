e653defdc0094e0291d64802c0a95b1de66033f1
Ola <Ola.Forsslund@gmail.com>
2021-04-16 23:42:59 +0200

    Stylus turns off BLE and pause execution after 5min with no connection. Wake by pressing button.

--- firmware/stylus_service.js
+++ firmware/stylus_service.js
@@ -16,34 +16,37 @@ const pin_dummy = D15; // Some unused pin
 const READ_INTERVAL_LOW = 1000;  // ms OBS device will not go to sleep if there is a timer that
                                  // will end within 1500 ms (according to 
                                  // https://www.espruino.com/Power+Consumption,seems incorrect...)
-const READ_INTERVAL_DEEP_SLEEP = 10000;  // ms
 const READ_INTERVAL_HI  = 20;    // ms
-const INACTIVE_THRESHOLD= 1000;  // number of READ_INTERVAL_HI before going to low activity mode
+const INACTIVE_THRESHOLD= 1500;  // number of READ_INTERVAL_HI before going to low activity mode
 
-var a=1;
-var b=0;
-var start=true;
 let StylusData = new Uint8Array(2);
 let spi = false;
+var MODE_SLEEP=true;  // Initiate to true so that BLE will be initated at first update
+var timeoutToken;
 
-NRF.setServices({
-  "90ad0000-662b-4504-b840-0ff1dd28d84e": {
-    "90ad0001-662b-4504-b840-0ff1dd28d84e": {
-      notify: true,
-      value : StylusData,
-      maxLen : 2,
-      readable : true,
-      writable : true,
+function activateBLE(){
+  MODE_SLEEP=false;
+  
+  // Change the name that's adveprtised
+  NRF.wake();
+  NRF.setAdvertising({}, {name:"Polhem Stylus"});
+  
+  NRF.setServices({
+    "90ad0000-662b-4504-b840-0ff1dd28d84e": {
+      "90ad0001-662b-4504-b840-0ff1dd28d84e": {
+        notify: true,
+        value : StylusData,
+        maxLen : 2,
+        readable : true,
+        writable : true,
 
-      onWrite : function(evt) {
-        start=true;
+        onWrite : function(evt) {
+          //start=true;
+        }
       }
     }
-  }
-}, { uart : true });
-
-// Change the name that's adveprtised
-NRF.setAdvertising({}, {name:"Polhem Stylus"});
+  }, { uart : true });
+}
 
 getRotation = function() {
   let d=0;
@@ -60,79 +63,105 @@ getRotation = function() {
   return d;
 };
 
-getButtonState = function() {
-  return !digitalRead(pin_button);
+var btn=false;
+function getButtonState(){
+  return btn;
+  //return !digitalRead(pin_button);
+}
+
+var buttonActivity = function(state){
+  LED2.write(state.state);  
+  btn=state.state;
+  updatePossition();
 };
+
+// Watch button one
+setWatch(buttonActivity, BTN1, {repeat:true, edge:"both", debounce:"10"});
+
+// Watch button two
+pinMode(pin_button, 'input_pullup');
+setWatch(buttonActivity, pin_button, {repeat:true, edge:"both", debounce:"10"});
 
 //var on = false;
 var inactiveCounter = 0;               // Keeps track of activity
 var readModeHi = true;
+
+var updateInterval=READ_INTERVAL_HI;
+var preRotation = 1;
+var preButton = 0;
 function updatePossition()
 {
-  var updateInterval=READ_INTERVAL_HI;
-  if(start){
-    
-    var preA=a;
-    var preB=b;        
-    // Only sample possition if connected
-    if( NRF.getSecurityStatus().connected==true )
-    {
-      a = getRotation();
-    }
-    b = getButtonState();
+  if(MODE_SLEEP) activateBLE();
 
-    // Only send data if new information
-    // else count up inactive counter
-    if( a!=preA || b!=preB )
-    {      
-      inactiveCounter = 0;
-      
-      // Pack message
-      StylusData[0] = a>>8;
-      StylusData[1] = a - 256*StylusData[0];
+  var rotation=preRotation;
+  var button=preButton;
+  // Only sample possition if connected
+  if( NRF.getSecurityStatus().connected==true )
+  {
+    rotation = getRotation();
+  }
+  button = getButtonState();
 
-      if(b) StylusData[0] = StylusData[0] + 128;
+  // Only send data if new information
+  // else count up inactive counter
+  if( rotation!=preRotation || button!=preButton )
+  {
+    updateInterval = READ_INTERVAL_HI;
+    preRotation=rotation;
+    preButton=button;
+    // Activity
+    inactiveCounter = 0;
 
-      NRF.updateServices({
-        "90ad0000-662b-4504-b840-0ff1dd28d84e": {
-          "90ad0001-662b-4504-b840-0ff1dd28d84e": {
-            value: StylusData,
-            notify: true
-          }
+    // Pack message
+    StylusData[0] = rotation>>8;
+    StylusData[1] = rotation - 256*StylusData[0];
+    if(button) StylusData[0] = StylusData[0] + 128;
+
+    // Send data
+    NRF.updateServices({
+      "90ad0000-662b-4504-b840-0ff1dd28d84e": {
+        "90ad0001-662b-4504-b840-0ff1dd28d84e": {
+          value: StylusData,
+          notify: true
         }
-      });
-    }
-    else{
-      inactiveCounter++;
-      if( inactiveCounter > INACTIVE_THRESHOLD ){
-        inactiveCounter = INACTIVE_THRESHOLD; // Avoid counter owerflow
-        // Reduced read interval
-        if( NRF.getSecurityStatus().connected==true ){
-            updateInterval = READ_INTERVAL_LOW;
-        }
-        else{
-            updateInterval = READ_INTERVAL_DEEP_SLEEP;
-        }
+      }
+    });
+  }
+  else{
+    // No activity
+    inactiveCounter++;
+    if( inactiveCounter > INACTIVE_THRESHOLD ){
+      inactiveCounter = INACTIVE_THRESHOLD; // Avoid counter owerflow
+      // Reduced read interval
+      if( NRF.getSecurityStatus().connected==true ){
+          updateInterval = READ_INTERVAL_LOW;
+      }
+      else{
+        // No activity and no connection, 'sleep' (inactivate BLE)
+        // Should not need to do this, according to the below statement!
+        //
+        // nRF52 devices are used in newer Espruino products. On these, Espruino is able to enter low power
+        // sleep modes automatically. Out of the box, power consumption is around 20uA (0.02mA) or over a 
+        // year on a CR2032 battery.
+          NRF.sleep();
+          MODE_SLEEP=true;
       }
     }
   }
-  setTimeout(updatePossition, updateInterval);
+  if( !MODE_SLEEP ){
+    clearTimeout(timeoutToken);  // Make sure we only have one active timeout
+    timeoutToken = setTimeout(updatePossition, updateInterval);
+  }
+  //LED1.write(MODE_SLEEP);
 }
 
-setTimeout(updatePossition, READ_INTERVAL_HI); // Initial call
+timeoutToken = setTimeout(updatePossition, READ_INTERVAL_HI); // Initial call
 
-
+//
+// Is this the power hog? Should we disable SPI when not connected?
+//
 // Start after 1 s to let other things start first....
 setTimeout(function() {
   spi = new SPI(); // Software spi since we need nonstandard bit length (11)
   spi.setup({mosi:pin_dummy, miso:pin_green_to_sensor_data, sck:pin_white_to_sensor_clk, mode:2, bits:11});
 },1000);
-
-pinMode(pin_button, 'input_pullup');
-var b2 = function(state){
-  LED2.write(!state.state);
-};
-
-setWatch(b2, pin_button, {repeat:true, edge:"both", debounce:"10"});
-setSleepIndicator(LED1);
-//setDeepSleep(1);
